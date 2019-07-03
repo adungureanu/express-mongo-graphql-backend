c3a677355d21c03f30804408b7517f07bb52e499
Ungureanu,Adrian <Adrian.Ungureanu@Cerner.com>
2019-07-03 14:00:11 +0300

    backend finish

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,4 +1,5 @@
 const Event = require('../../models/event');
+const User = require('../../models/user')
 const { transformEvent } = require('./merge')
 
 
@@ -13,19 +14,22 @@ module.exports = {
           throw err;
         }
       },   
-      createEvent: async args => {
+      createEvent: async (args, req)  => {
+        if(!req.isAuth){
+            throw new Error('Unautheticated!')
+        }
         const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
           date: new Date(args.eventInput.date),
-                creator: '5d1b0f941d50675660512523'
+          creator: req.userId
             }); 
             let createdEvent;
             try{
                 const result = await event.save();
                 createdEvent = transformEvent(result);
-                const creator = await User.findById('5d1b0f941d50675660512523');
+                const creator = await User.findById(req.userId);
 
                 if(!creator){
                     throw new Error('User not found.');
